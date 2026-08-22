152b53bafbff6848cd3835f1faac4051c4f18968
Muhammad Saleem <maliksaleem1993@gmail.com>
2026-08-22 17:05:24 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
    returning users get the update.
    ========================================================================== */
 
-const CACHE_VERSION = 'gharsaz360-v3.0.0';
+const CACHE_VERSION = 'gharsaz360-v3.1.0';
 const APP_SHELL = [
   './',
   './index.html',
